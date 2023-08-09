00c009afb1e3100013cc55048b0e77cea9216e01
parthgupta7800 <122226563+parthgupta7800@users.noreply.github.com>
2023-08-09 16:29:53 +0530

    Area of the traingle

---
+++ area_of_traingle.java
@@ -0,0 +1,16 @@
+
+//PARTH GUPTA
+//2215001214
+import java.util.*;
+
+class traingle {
+    public static void main(String args[]) {
+        Scanner ob = new Scanner(System.in);
+        System.out.println("Enter the height");
+        int h = ob.nextInt();
+        System.out.println("Enter the base");
+        int b = ob.nextInt();
+        double a = 0.5 * h * b;
+        System.out.println(a);
+    }
+}

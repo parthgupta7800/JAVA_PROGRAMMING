d876f6b1f4f1cfe7f64ca1c5dedccbb91760c0f2
parthgupta7800 <122226563+parthgupta7800@users.noreply.github.com>
2023-08-09 16:29:16 +0530

    Area of the Circle

---
+++ area_of_circle.java
@@ -0,0 +1,14 @@
+
+//PARTH GUPTA
+//2215001214
+import java.util.*;
+
+class circle {
+    public static void main(String args[]) {
+        Scanner ob = new Scanner(System.in);
+        System.out.println("Enter the radius of the circle");
+        int r = ob.nextInt();
+        double a = 3.14 * r * r;
+        System.out.println(a);
+    }
+}

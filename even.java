ea54cc80572c11669c9db6a4ac53a73d10671f9f
parthgupta7800 <122226563+parthgupta7800@users.noreply.github.com>
2023-08-09 16:31:34 +0530

    number is even or not

---
+++ even.java
@@ -0,0 +1,17 @@
+
+//PARTH GUPTA
+//2215001214
+import java.util.*;
+
+class even {
+    public static void main(String args[]) {
+        Scanner ob = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int a = ob.nextInt();
+        if (a % 2 == 0) {
+            System.out.println("a is even");
+        } else
+            System.out.println("a is odd");
+
+    }
+}

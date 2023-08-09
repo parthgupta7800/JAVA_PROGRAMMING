bf6f0e37b4d3438c6823fb218eb6f57b3c243d5b
parthgupta7800 <122226563+parthgupta7800@users.noreply.github.com>
2023-08-09 16:28:42 +0530

    Addition of two numbers

---
+++ addition.java
@@ -0,0 +1,15 @@
+
+//PARTH GUPTA
+//2215001214
+import java.util.*;
+
+class add {
+    public static void main(String args[]) {
+        Scanner ob = new Scanner(System.in);
+        System.out.println("Enter the two number to be added");
+        int a = ob.nextInt();
+        int b = ob.nextInt();
+        int c = a + b;
+        System.out.println(c);
+    }
+}

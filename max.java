eba1f7cc9c75857eacbf8ffcc262f90a56714080
parthgupta7800 <122226563+parthgupta7800@users.noreply.github.com>
2023-08-09 16:33:43 +0530

    Maximum Number between the two

---
+++ max.java
@@ -0,0 +1,17 @@
+
+//PARTH GUPTA
+//2215001214
+import java.util.*;
+
+class max {
+    public static void main(String args[]) {
+        Scanner ob = new Scanner(System.in);
+        System.out.println("Enter the two number");
+        int a = ob.nextInt();
+        int b = ob.nextInt();
+        if (a > b)
+            System.out.println(a + " is greater");
+        else
+            System.out.println(b + " is greater");
+    }
+}

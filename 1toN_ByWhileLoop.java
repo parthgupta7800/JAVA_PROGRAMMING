78adc9ed823dfa2c07acfb12221adb1c79ef435b
parthgupta7800 <122226563+parthgupta7800@users.noreply.github.com>
2023-08-09 16:25:21 +0530

    1 to N Natural number using While Loop

---
+++ 1toN_ByWhileLoop.java
@@ -0,0 +1,18 @@
+
+//PARTH GUPTA
+//2215001214
+import java.util.*;
+
+class natural {
+    public static void main(String args[]) {
+        Scanner ob = new Scanner(System.in);
+        int n;
+        System.out.println("Enter the limit that is value of n");
+        n = ob.nextInt();
+        int i = 1;
+        while (i <= n) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}

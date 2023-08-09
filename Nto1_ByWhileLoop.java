af760c7fa96cb4c0ac3e96b04e246e4d64d652d1
parthgupta7800 <122226563+parthgupta7800@users.noreply.github.com>
2023-08-09 16:34:08 +0530

    N to 1 using While Loop

---
+++ Nto1_ByWhileLoop.java
@@ -0,0 +1,17 @@
+
+//PARTH GUPTA
+//2215001214
+import java.util.*;
+
+class natural1 {
+    public static void main(String args[]) {
+        Scanner ob = new Scanner(System.in);
+        int n;
+        System.out.println("Enter the Number");
+        n = ob.nextInt();
+        while (n > 0) {
+            System.out.println(n);
+            n--;
+        }
+    }
+}

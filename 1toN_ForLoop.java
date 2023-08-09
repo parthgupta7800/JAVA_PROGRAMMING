9f906d9e77d704720a27a7123b5c1a46abddc4bb
parthgupta7800 <122226563+parthgupta7800@users.noreply.github.com>
2023-08-09 16:26:08 +0530

    1 to N Natural number using For Loop

---
+++ 1toN_ForLoop.java
@@ -0,0 +1,16 @@
+
+//PARTH GUPTA
+//2215001214
+import java.util.*;
+
+class natural2 {
+    public static void main(String args[]) {
+        Scanner ob = new Scanner(System.in);
+        int n, i;
+        System.out.println("Enter the number");
+        n = ob.nextInt();
+        for (i = 1; i <= n; i++) {
+            System.out.println(i);
+        }
+    }
+}
